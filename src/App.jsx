08cdaa0084311170e0ebb6a80726c7be2a8c134e
Mohammed Hayath <mdhayath64@gmail.com>
2026-06-27 15:12:21 +0530

    initial test react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Vite + React CI/CD Trigger test, If you see this CI/CD changes were successful</h1>
+      <p>Sample project scaffolded by Me.</p>
+      <button onClick={() => alert('Hello from Vite + React!')}>Click me</button>
+    </div>
+  )
+}
