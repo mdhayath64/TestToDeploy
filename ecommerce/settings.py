330e2ca2c4ede016f0fb56bdbe042c15a517db71
Mohammed Hayath <mdhayath64@gmail.com>
2026-07-27 00:10:41 +0530

    allow all ip

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-)n-9096sh)l3crl-ssb58korw(x(#t221s^!_(sq%u71gqs4ro
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['api.tradestreaks.lol','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['api.tradestreaks.lol','localhost','127.0.0.1','*']
 
 
 # Application definition
