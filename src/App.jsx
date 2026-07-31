d3c881d0fdc34aba5fb5d430bf3cd82b0740ed29
Mohammed Hayath <mdhayath64@gmail.com>
2026-08-01 02:11:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App() {
   return (
     <div className="app">
-      <h1>Vite + React CI CD test Hayath</h1>
+      <h1>Vite + React CI CD test Hayath with PR</h1>
       <p>Sample project scaffolded by Me.</p>
       <button onClick={() => alert('Hello from Vite + React!')}>Click me</button>
     </div>
